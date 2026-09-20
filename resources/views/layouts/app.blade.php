2b3ce756d9c945b3cc541a1d1fed0cd61076a80d
Dilmith-Ranasinghe518 <it23249998@my.sliit.lk>
2026-09-20 22:14:21 +0530

    fix: generate valid multi-resolution favicon.ico and add apple-touch-icon and PNG favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,11 @@
             }
         @endphp
         <title>{{ $pageTitle }}</title>
-        <link rel="icon" href="{{ asset('loops-icon.png') }}" type="image/png">
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -4,7 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Maintenance Mode - LoopsHR</title>
-        <link rel="icon" href="{{ asset('loops-icon.png') }}" type="image/png">
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
